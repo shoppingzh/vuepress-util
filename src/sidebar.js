42e9dc70ab46a2d26b750bbc54f223ea404688cf
shoppingzh <1049262057@qq.com>
2021-10-08 17:52:13 +0800

    优化自动生成侧边栏并支持文档排序

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,50 +1,55 @@
 import { resolve, join } from 'path'
-import { readdirSync } from 'fs'
-import { listFiles } from './file'
+import { readdirSync, readFileSync } from 'fs'
+import { listFiles, getExtension, getBasename } from './file'
 
 const docPath = resolve(process.cwd(), 'docs')
 
-function listDocDirDeep() {
-  const doList = (path = '', depth = 0) => {
-    const dirs = readdirSync(join(docPath, path), {
-      withFileTypes: true
-    }).filter(f => f.isDirectory()).filter(f => !/^\./.test(f.name)).map(f => ({
-      name: f.name,
-      path: join(path, f.name)
-    }))
-    dirs.forEach(subdir => {
-      subdir.children = doList(subdir.path, depth + 1)
-    })
-    return dirs
-  }
-  return doList()
-}
-
-function listLeafDir(dirs) {
-  const leafDirs = []
-  const doList = (dirs) => {
-    for (const dir of dirs) {
-      const children = dir.children
-      if (!children ||!children.length) {
-        leafDirs.push(dir.path)
-      } else {
-        doList(children)
-      }
-    }
-  }
-  doList(dirs)
-  return leafDirs
-}
-
-export function generateSidebar() {
-  const dirs = listDocDirDeep()
-  const leafDirs = listLeafDir(dirs)
-  return leafDirs.reduce((conf, dir) => {
-    const files =  listFiles(join(docPath, dir))
-    conf[join('/', dir, '/')] = files.map(f => ([
-      f.name,
-      f.name
-    ]))
-    return conf
+function getOrders(content) {
+  if (!content) return {}
+  const lines = content.split(/[\n|\r\n]/)
+  return lines.filter(line => line.trim()).reduce((sort, line, index) => {
+    sort[line.trim()] = index + 1
+    return sort
   }, {})
+}
+
+/**
+ * 生成sidebar配置
+ */
+export function generateSidebar() {
+  /**
+   * 1. 递归读取docs目录下的次级目录
+   * 2. 如果目录下没有文档，则不生成侧边栏
+   * 3. 如果目录下有文档，则根据是否有.order文件来决定其顺序
+   */
+  const conf = {}
+  const doDeepList = (path = '') => {
+    const dirs = readdirSync(join(docPath, path), { withFileTypes: true })
+      .filter(o => o.isDirectory()) // 目录
+      .filter(f => !/^\./.test(f.name)) // 非以.开头的目录
+    dirs.forEach(dir => {
+      const dirPath = join(path, dir.name, '/')
+      const files = listFiles(join(docPath, dirPath))
+      const docFiles = files.filter(f => /md/i.test(getExtension(f.name)))
+      if (docFiles.length) {
+        const orderFile = files.find(f => /^\.order$/i.test(f.name))
+        if (orderFile) {
+          const content = readFileSync(join(docPath, dirPath, orderFile.name)).toString()
+          const orders = getOrders(content)
+          docFiles.sort((a, b) => {
+            const ao = orders[a.name] || orders[getBasename(a.name)] || 0
+            const bo = orders[b.name] || orders[getBasename(b.name)] || 0
+            return ao - bo
+          })
+        }
+        conf[dirPath] = docFiles.map(f => ([
+          f.name,
+          f.name
+        ]))
+      }
+      doDeepList(join(path, dir.name))
+    })
+  }
+  doDeepList()
+  return conf
 }
